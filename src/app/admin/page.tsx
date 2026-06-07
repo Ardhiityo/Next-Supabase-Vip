8a6247043c1ba23835397f06360eb746d007fb57
Ardhityo <aryaadi229@gmail.com>
2026-06-07 17:24:24 +0700

    6.Update Data

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -19,7 +19,6 @@ import {
 } from "@/components/ui/dropdown-menu";
 import {
   Dialog,
-  DialogClose,
   DialogContent,
   DialogDescription,
   DialogFooter,
@@ -27,14 +26,6 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
-import { Input } from "@/components/ui/input";
-import { Field, FieldGroup, FieldLabel } from "@/components/ui/field";
-import { Button } from "@/components/ui/button";
-import { Label } from "@/components/ui/label";
-import { Ellipsis } from "lucide-react";
-import Image from "next/image";
-import { createClient } from "@/utils/supabase/client";
-import { Textarea } from "@/components/ui/textarea";
 import {
   Select,
   SelectContent,
@@ -43,9 +34,6 @@ import {
   SelectValue,
   SelectTrigger,
 } from "@/components/ui/select";
-import { useEffect, useState, type SubmitEvent } from "react";
-import { IMenu } from "@/types/IMenu";
-import { toast } from "sonner";
 import {
   AlertDialog,
   AlertDialogAction,
@@ -56,12 +44,24 @@ import {
   AlertDialogHeader,
   AlertDialogTitle,
 } from "@/components/ui/alert-dialog";
+import { Input } from "@/components/ui/input";
+import { Field, FieldGroup, FieldLabel } from "@/components/ui/field";
+import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label";
+import { Ellipsis } from "lucide-react";
+import { createClient } from "@/utils/supabase/client";
+import { Textarea } from "@/components/ui/textarea";
+import { useEffect, useState, type SubmitEvent } from "react";
+import { IMenu } from "@/types/IMenu";
+import { toast } from "sonner";
+import Image from "next/image";
 
 export default function Page() {
   const [menus, setMenus] = useState<IMenu[]>([]);
   const [createDialogOpen, setCreateDialogOpen] = useState(false);
+  const [editDialogOpen, setEditDialogOpen] = useState(false);
   const [deleteDialogOpen, setDeleteDialogOpen] = useState(false);
-  const [menuToDelete, setMenuToDelete] = useState<IMenu | null>(null);
+  const [selectedMenu, setSelectedMenu] = useState<IMenu | null>(null);
   const supabase = createClient();
 
   useEffect(() => {
@@ -91,18 +91,41 @@ export default function Page() {
   }
 
   async function handleDelete() {
-    if (!menuToDelete) return;
+    if (!selectedMenu) return;
 
     const { error } = await supabase
       .from("menus")
       .delete()
-      .eq("id", menuToDelete.id);
+      .eq("id", selectedMenu.id);
 
     if (!error) {
-      setMenus(menus.filter((menu) => menu.id !== menuToDelete.id));
+      setMenus(menus.filter((menu) => menu.id !== selectedMenu.id));
       setDeleteDialogOpen(false);
-      setMenuToDelete(null);
+      setSelectedMenu(null);
       toast("Menu deleted successfully");
+    }
+  }
+
+  async function handleUpdate(e: SubmitEvent<HTMLFormElement>) {
+    e.preventDefault();
+    if (!selectedMenu) return;
+
+    const formData = new FormData(e.currentTarget);
+    const newMenu = Object.fromEntries(formData.entries());
+
+    const { error } = await supabase
+      .from("menus")
+      .update(newMenu)
+      .eq("id", selectedMenu.id);
+
+    if (!error) {
+      const newMenus = menus.map((menu) =>
+        menu.id === selectedMenu.id ? { ...menu, ...newMenu } : menu,
+      );
+      setMenus(newMenus);
+      setEditDialogOpen(false);
+      setSelectedMenu(null);
+      toast("Menu updated successfully");
     }
   }
 
@@ -110,18 +133,17 @@ export default function Page() {
     <>
       <div className="flex justify-between">
         <h1 className="text-3xl font-bold mb-16">Menu</h1>
+        {/* Create Dialog */}
         <Dialog open={createDialogOpen} onOpenChange={setCreateDialogOpen}>
           <DialogTrigger asChild>
             <Button type="button">Add Menu</Button>
           </DialogTrigger>
           <DialogContent className="sm:max-w-sm">
+            <DialogHeader>
+              <DialogTitle>Add Menu</DialogTitle>
+              <DialogDescription>Make new menu to here.</DialogDescription>
+            </DialogHeader>
             <form onSubmit={handleAddMenu}>
-              <DialogHeader>
-                <DialogTitle>Add Menu</DialogTitle>
-                <DialogDescription>
-                  Make new menu to here. Click save when you&apos;re done.
-                </DialogDescription>
-              </DialogHeader>
               <FieldGroup>
                 <Field>
                   <Label htmlFor="name">Name</Label>
@@ -166,25 +188,9 @@ export default function Page() {
             </form>
           </DialogContent>
         </Dialog>
-
-        <AlertDialog open={deleteDialogOpen} onOpenChange={setDeleteDialogOpen}>
-          <AlertDialogContent>
-            <AlertDialogHeader>
-              <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
-              <AlertDialogDescription>
-                This action cannot be undone. This will permanently delete the
-                menu <strong>{menuToDelete?.name}</strong> from our servers.
-              </AlertDialogDescription>
-            </AlertDialogHeader>
-            <AlertDialogFooter>
-              <AlertDialogCancel>Cancel</AlertDialogCancel>
-              <AlertDialogAction onClick={handleDelete}>
-                Delete
-              </AlertDialogAction>
-            </AlertDialogFooter>
-          </AlertDialogContent>
-        </AlertDialog>
+        {/* Create Dialog */}
       </div>
+
       <section>
         <Table>
           <TableHeader>
@@ -229,11 +235,18 @@ export default function Page() {
                       <DropdownMenuGroup>
                         <DropdownMenuLabel>Actions</DropdownMenuLabel>
                         <DropdownMenuSeparator />
-                        <DropdownMenuItem>Edit</DropdownMenuItem>
+                        <DropdownMenuItem
+                          onClick={() => {
+                            setSelectedMenu(menu);
+                            setEditDialogOpen(true);
+                          }}
+                        >
+                          Edit
+                        </DropdownMenuItem>
                         <DropdownMenuItem
                           className="text-red-600"
                           onClick={() => {
-                            setMenuToDelete(menu);
+                            setSelectedMenu(menu);
                             setDeleteDialogOpen(true);
                           }}
                         >
@@ -248,6 +261,92 @@ export default function Page() {
           </TableBody>
         </Table>
       </section>
+
+      {/* Delete Dialog */}
+      <AlertDialog open={deleteDialogOpen} onOpenChange={setDeleteDialogOpen}>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+            <AlertDialogDescription>
+              This action cannot be undone. This will permanently delete the
+              menu <strong>{selectedMenu?.name}</strong> from our servers.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>Cancel</AlertDialogCancel>
+            <AlertDialogAction onClick={handleDelete}>Delete</AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+
+      {/* Edit Dialog */}
+      <Dialog open={editDialogOpen} onOpenChange={setEditDialogOpen}>
+        <DialogContent className="sm:max-w-sm">
+          <form onSubmit={handleUpdate}>
+            <DialogHeader>
+              <DialogTitle>Edit Menu</DialogTitle>
+              <DialogDescription>Edit the menu details.</DialogDescription>
+            </DialogHeader>
+            <FieldGroup>
+              <Field>
+                <Label htmlFor="edit-name">Name</Label>
+                <Input
+                  id="edit-name"
+                  required
+                  name="name"
+                  defaultValue={selectedMenu?.name}
+                />
+              </Field>
+              <Field>
+                <Label htmlFor="edit-description">Description</Label>
+                <Textarea
+                  id="edit-description"
+                  name="description"
+                  className="resize-none"
+                  required
+                  defaultValue={selectedMenu?.description}
+                />
+              </Field>
+              <Field>
+                <Label htmlFor="edit-price">Price</Label>
+                <Input
+                  id="edit-price"
+                  name="price"
+                  required
+                  defaultValue={selectedMenu?.price}
+                />
+              </Field>
+              <Field>
+                <Label htmlFor="edit-image">Image</Label>
+                <Input
+                  id="edit-image"
+                  name="image"
+                  required
+                  defaultValue={selectedMenu?.image}
+                />
+              </Field>
+              <Field>
+                <FieldLabel>Category</FieldLabel>
+                <Select name="category" defaultValue={selectedMenu?.category}>
+                  <SelectTrigger>
+                    <SelectValue placeholder="Select a category" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectGroup>
+                      <SelectItem value="coffee">Coffee</SelectItem>
+                      <SelectItem value="non-coffee">Non Coffee</SelectItem>
+                      <SelectItem value="pastry">Pastry</SelectItem>
+                    </SelectGroup>
+                  </SelectContent>
+                </Select>
+              </Field>
+            </FieldGroup>
+            <DialogFooter showCloseButton>
+              <Button type="submit">Save changes</Button>
+            </DialogFooter>
+          </form>
+        </DialogContent>
+      </Dialog>
     </>
   );
 }
